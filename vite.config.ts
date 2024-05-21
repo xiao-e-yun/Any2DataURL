7cc209e0739ecfa518185b261db4e0395354800b
xiaoeyun <67814228+xiao-e-yun@users.noreply.github.com>
2024-05-21 13:47:54 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '',
   css: {
     preprocessorOptions: {
       scss: { additionalData: `@use "./assets/style/color";@import "./assets/style/vars.scss";` },
